06e656c511cad3bf8d0a5085733890a6b45314c9
andyols <andyols.dev@gmail.com>
2021-01-21 16:10:19 -0600

    Wire up MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config({ path: '.env.local' })
+
+connectDB()
 
 const app = express()
 
@@ -21,5 +25,7 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`Server running on port ${PORT} (${process.env.NODE_ENV})`)
+  console.log(
+    `✔️  Express running on port ${PORT} (${process.env.NODE_ENV})`.cyan
+  )
 )
