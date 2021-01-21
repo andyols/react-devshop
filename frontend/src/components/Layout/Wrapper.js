f47896aa4385aaedca815b1229eeaaad78627484
andyols <andyols.dev@gmail.com>
2021-01-21 11:27:07 -0600

    Individual product display page

--- frontend/src/components/Layout/Wrapper.js
+++ frontend/src/components/Layout/Wrapper.js
@@ -2,7 +2,7 @@ import { Container, Stack } from '@chakra-ui/react'
 
 const MainWrapper = ({ children }) => (
   <Stack flex='1 1 0%' as='main'>
-    <Container maxW='4xl'>{children}</Container>
+    <Container maxW='5xl'>{children}</Container>
   </Stack>
 )
 
