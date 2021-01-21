3722e42b41e268aab6f02b6dc41da652b4d92394
andyols <andyols.dev@gmail.com>
2021-01-20 18:10:52 -0600

    Header styling and basic responsiveness

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -1,4 +1,12 @@
-import { Flex, Link, Text } from '@chakra-ui/react'
+import {
+  Flex,
+  HStack,
+  Icon,
+  Link,
+  Text,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { FiLogIn, FiShoppingBag } from 'react-icons/fi'
 import { ColorModeSwitcher } from '../ColorModeSwitcher'
 
 const Header = () => {
@@ -7,14 +15,27 @@ const Header = () => {
       as='header'
       justify='space-between'
       align='center'
-      px={5}
+      px='5%'
       py={3}
       boxShadow='base'
+      color={'gray.50'}
+      direction={['column', 'row']}
+      bg={useColorModeValue('red.400', 'gray.700')}
     >
       <Text as={Link} fontWeight='semibold' fontSize='xl'>
         {'>dev-shop'}
       </Text>
-      <ColorModeSwitcher alignSelf='flex-end' />
+      <HStack spacing={7}>
+        <Link as={HStack}>
+          <Text>Sign In</Text>
+          <Icon as={FiLogIn} />
+        </Link>
+        <Link as={HStack}>
+          <Text>Cart</Text>
+          <Icon as={FiShoppingBag} />
+        </Link>
+        <ColorModeSwitcher alignSelf='flex-end' />
+      </HStack>
     </Flex>
   )
 }
