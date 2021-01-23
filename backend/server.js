4e3640937e3c5de733cf1d1a13591f055cbdedfa
andyols <andyols.dev@gmail.com>
2021-01-23 17:17:47 -0600

    User auth route / controller

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 import express from 'express'
 import connectDB from './config/db.js'
 import productRoutes from './routes/product.js'
+import userRoutes from './routes/user.js'
 import { notFound, errorHandler } from './middleware/errors.js'
 import { logInfo } from './utils/logs.js'
 
@@ -10,10 +11,10 @@ dotenv.config({ path: '.env.local' })
 connectDB()
 
 const app = express()
-
-app.get('/', (req, res) => res.send('Hello World'))
+app.use(express.json())
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
